14c596827ee9eead2ae64d79357423ff9aa0b26d
Pentapati Seshagiri <Pentapati.Seshagiri@ivycomptech.com>
2024-10-19 09:12:23 +0530

    adding cluster for improving performance

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const cluster = require('cluster');
 
 function delay(duration) {
     const startTime = Date.now();
@@ -9,12 +10,21 @@ function delay(duration) {
     }
 }
 app.get("/", (req, res) => {
-    res.send("performance example")
+    res.send(`performance example: ${process.pid}`)
 })
+
+if (cluster.isMaster) {
+    console.log("seshu is running master");
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log("seshu is ogging workers");
+    app.listen(8000);
+}
 
 app.get('/timer', (req, res) => {
     delay(9000);
-    res.send('ding ding')
+    res.send(`ding ding: ${process.pid}`)
 })
 
-app.listen(8000);
+
