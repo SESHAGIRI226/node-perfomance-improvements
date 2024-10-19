b63a7dfd3f401494c15a93096f002aca7f71f798
Pentapati Seshagiri <Pentapati.Seshagiri@ivycomptech.com>
2024-10-19 13:29:21 +0530

    added pm2 for improving performance and moitoring

--- server.js
+++ server.js
@@ -1,33 +1,56 @@
+// const express = require('express');
+
+// const app = express();
+// const cluster = require('cluster');
+// const os = require('os');
+
+// function delay(duration) {
+//     const startTime = Date.now();
+//     while (Date.now() - startTime < duration) {
+
+//     }
+// }
+// app.get("/", (req, res) => {
+//     res.send(`performance example: ${process.pid}`)
+// })
+
+// if (cluster.isMaster) {
+//     console.log("seshu is running master");
+//     const num_Cpus = os.cpus().length;
+//     for (let i = 0; i < num_Cpus; i++) {
+//         cluster.fork();
+//     }
+// } else {
+//     console.log("seshu is ogging workers");
+//     app.listen(8000);
+// }
+
+// app.get('/timer', (req, res) => {
+//     delay(9000);
+//     res.send(`ding ding: ${process.pid}`)
+// })
+
+
 const express = require('express');
 
 const app = express();
-const cluster = require('cluster');
-const os = require('os');
 
 function delay(duration) {
     const startTime = Date.now();
     while (Date.now() - startTime < duration) {
-
+        //event loop is blocked...
     }
 }
-app.get("/", (req, res) => {
-    res.send(`performance example: ${process.pid}`)
-})
 
-if (cluster.isMaster) {
-    console.log("seshu is running master");
-    const num_Cpus = os.cpus().length;
-    for (let i = 0; i < num_Cpus; i++) {
-        cluster.fork();
-    }
-} else {
-    console.log("seshu is ogging workers");
-    app.listen(8000);
-}
+app.get('/', (req, res) => {
+    res.send(`Performance example: ${process.pid}`);
+});
 
 app.get('/timer', (req, res) => {
-    delay(9000);
-    res.send(`ding ding: ${process.pid}`)
-})
+    delay(4000);
+    res.send(`Beep beep beep! ${process.pid}`);
+});
 
-
+console.log('Running server.js...');
+console.log('Worker process started.');
+app.listen(3000);
